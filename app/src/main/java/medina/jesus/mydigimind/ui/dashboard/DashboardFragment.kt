e8685edeabdde2471583f95001a02a8b3ee61ea6
Jesus <159750556+thinkaboutj@users.noreply.github.com>
2025-02-18 23:33:08 -0700

    ojala ya sirva

--- app/src/main/java/medina/jesus/mydigimind/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/medina/jesus/mydigimind/ui/dashboard/DashboardFragment.kt
@@ -6,37 +6,29 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import medina.jesus.mydigimind.R
 import medina.jesus.mydigimind.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var dashboardViewModel: DashboardViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        dashboardViewModel =
+            ViewModelProvider(owner = this).get(DashboardViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container,false)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
+        })
+
         return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
